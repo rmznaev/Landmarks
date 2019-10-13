79cd531c9ec6c043c9c3e536f1f5ebb4190ceff6
Ramazan Abdullayev <ramazan.aev@gmail.com>
2019-10-13 12:32:04 +0400

    Working on text fields

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -10,7 +10,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        Text("Turtle Rock")
+            .font(.title)
+            .foregroundColor(.black)
+            .multilineTextAlignment(.center)
     }
 }
 
