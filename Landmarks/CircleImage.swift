e7bd566ed1fd6490cb53db25e5a4a960092f1c66
Ramazan Abdullayev <ramazan.aev@gmail.com>
2019-10-13 12:53:10 +0400

    Created circle image view with using SwiftUI

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,24 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Ramazan Abdullayev on 10/13/19.
+//  Copyright © 2019 Ramazan Abdullayev. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 10)
+    }
+}
